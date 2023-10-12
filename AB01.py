06213c1e8c3bd676b41551f02cf26e9e06ebc699
Anne Schmidt <136587108+anneeier@users.noreply.github.com>
2023-10-12 23:06:35 +0200

    pushg

---
+++ AB01.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+import numpy as np
+
+def sepia(img2sepia):
+    imgGray = cv.cvtColor(imgSrc, cv.COLOR_RGB2GRAY)
+    normalizedGary = np.array(imgGray, np.float32)/255
+    sepia = np.ones(imgSrc.shape)
+    sepia[:,:,0] *= 100 #B
+    sepia[:,:,1] *= 200 #G
+    sepia[:,:,2] *= 255 #R
+    
+    sepia[:,:,0] *= normalizedGary #B
+    sepia[:,:,1] *= normalizedGary #G
+    sepia[:,:,2] *= normalizedGary #R
+
+    return np.array(sepia, np.uint8)
+
+
+def saturation(imgSat):
+    imgHSV = cv.cvtColor(imgSat, cv.COLOR_BGR2HSV)
+    factor = 0.5
+    imgHSV[:,:,0] = imgHSV[:,:,0] * factor #B
+    imgHSV[:,:,1] = imgHSV[:,:,1] * factor #G
+    #imgHSV[:,:,2] = imgHSV[:,:,2] * factor #R
+    imgNew = cv.cvtColor(imgHSV, cv.COLOR_HSV2BGR)
+    return imgNew
+
+def vigniette(img):
+    
+    return 0
+
+
+imgSrc = cv.imread('Utils/Landschaft.jpg')
+imgGray = cv.cvtColor(imgSrc, cv.COLOR_RGB2GRAY)
+imgSepia = sepia(imgSrc)
+imgSat = saturation(imgSepia)
+
+
+cv.imshow("imgSrc",imgSrc)
+cv.imshow("imgGray",imgGray)
+cv.imshow("Sepia", imgSepia)
+cv.imshow("Saturation", imgSat)
+cv.waitKey(0)
+
+cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV1/Abgaben/AB01/imgSrc.png", imgSrc)
+cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV1/Abgaben/AB01/imgGray.png", imgGray)
+cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV1/Abgaben/AB01/imgSepia.png", imgSepia)
+cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV1/Abgaben/AB01/imgSat.png", imgSat)
+
+#C:\Users\annes\Documents\Studium\Aktuelle Module\BV1\Abgaben\AB01 - Bilder
+#Quellen:
+#https://gist.github.com/FilipeChagasDev/bb63f46278ecb4ffe5429a84926ff812
+#ChatGTP
+#https://www.pinterest.de/pin/760897299516483533/
