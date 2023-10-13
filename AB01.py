01919e260508646593efebb13da6107a59652656
Anne Schmidt <136587108+anneeier@users.noreply.github.com>
2023-10-13 13:30:19 +0200

    vig fertig

--- AB01.py
+++ AB01.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import math 
 
 def sepia(img2sepia):
     imgGray = cv.cvtColor(imgSrc, cv.COLOR_RGB2GRAY)
@@ -25,27 +26,44 @@ def saturation(imgSat):
     imgNew = cv.cvtColor(imgHSV, cv.COLOR_HSV2BGR)
     return imgNew
 
-def vigniette(img):
-    
-    return 0
+
+def vigniette(imgSrc, radius):    
+    width, height = imgSrc.shape
+    imgSrc = imgSrc/255
+    result = imgSrc
+    max_len = math.sqrt(width//2 * width//2 + height//2 * height//2)
+    for y in range(height):
+        for x in range(width):
+            tmp_x = width//2 - x
+            tmp_y = height//2 - y
+            tmp_len = math.sqrt(tmp_x*tmp_x + tmp_y*tmp_y) 
+            if tmp_len > radius:
+                reduceBrigt = (tmp_len - radius)/ max_len
+                result[x,y] = imgSrc[x,y] - max(0, reduceBrigt) 
+    return result
 
 
-imgSrc = cv.imread('Utils/SetGame.png')
+img_white = np.ones((500,500))
+
+imgSrc = cv.imread('Utils/LennaCol.png')
+
 imgGray = cv.cvtColor(imgSrc, cv.COLOR_RGB2GRAY)
 imgSepia = sepia(imgSrc)
 imgSat = saturation(imgSepia)
-
+imgVig = vigniette(imgGray, 150)
 
 cv.imshow("imgSrc",imgSrc)
 cv.imshow("imgGray",imgGray)
 cv.imshow("Sepia", imgSepia)
 cv.imshow("Saturation", imgSat)
+cv.imshow('Vig', imgVig)
 cv.waitKey(0)
 
-cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV2/Abgaben/AB01-Results/imgSrc.png", imgSrc)
-cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV2/Abgaben/AB01-Results/imgGray.png", imgGray)
-cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV2/Abgaben/AB01-Results/imgSepia.png", imgSepia)
-cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV2/Abgaben/AB01-Results/imgSat.png", imgSat)
+
+#cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV2/Abgaben/AB01-Results/imgSrc.png", imgSrc)
+#cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV2/Abgaben/AB01-Results/imgGray.png", imgGray)
+#cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV2/Abgaben/AB01-Results/imgSepia.png", imgSepia)
+#cv.imwrite("C:/Users/annes/Documents/Studium/Aktuelle Module/BV2/Abgaben/AB01-Results/imgSat.png", imgSat)
 
 #C:\Users\annes\Documents\Studium\Aktuelle Module\BV1\Abgaben\AB01 - Bilder
 #Quellen:
