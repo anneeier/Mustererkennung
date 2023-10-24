bd851159844f3d603586e451b962ee167ea584e5
Anne Schmidt <136587108+anneeier@users.noreply.github.com>
2023-10-24 02:39:39 +0200

    #AB02 angefangen

--- AB02.py
+++ AB02.py
@@ -1,4 +1,26 @@
 import cv2 
 import numpy as np
+from skimage.metrics import structural_similarity as ssim
 
-img = cv2.imread()
+#Ähnlichkeit von Bildern: SSIM
+
+img = cv2.imread('Utils/LennaCol.png')
+imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+result = ssim(imgGray, imgGray)
+
+print(result)
+
+
+#Steganographie: Binärbild in Blau-Kanal
+
+grayImg = cv2.imread('Utils/th.jpg', cv2.IMREAD_GRAYSCALE)
+
+bitImages = []
+for bit in range(8):
+    bitMask = 1 << bit
+    bitPlane = cv2.bitwise_and(grayImg, bitMask)
+    bitImages.append(bitPlane)
+
+for i, bitImages in enumerate(bitImages):
+    cv2.imwrite(f'bit_{i}.png', bitImages)
