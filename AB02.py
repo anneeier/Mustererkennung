32efeb55c4ed851530a483aecf534c787a104eac
Anne Schmidt <136587108+anneeier@users.noreply.github.com>
2023-10-26 17:55:24 +0200

    #AB02

--- AB02.py
+++ AB02.py
@@ -35,35 +35,35 @@ def blur(img):
     return blurImg
 
 
-img = cv2.imread('Utils/LennaCol.png')
-imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-bright_contrast_img = contrast(imgGray, 1.935)
-dark_contrast_img = contrast(imgGray, 0.485)
-bright_luminance_img = luminance(imgGray, 115)
-dark_luminance_img = luminance(imgGray, -51.5)
-blur_img = blur(imgGray)
+# img = cv2.imread('Utils/LennaCol.png')
+# imgGray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+# bright_contrast_img = contrast(imgGray, 1.935)
+# dark_contrast_img = contrast(imgGray, 0.485)
+# bright_luminance_img = luminance(imgGray, 115)
+# dark_luminance_img = luminance(imgGray, -51.5)
+# blur_img = blur(imgGray)
 
-cv2.imshow('Originial', imgGray)
-cv2.imshow('Bright Contrast', bright_contrast_img)
-cv2.imshow('Dark Contrast', dark_contrast_img)
-cv2.imshow('Blur', blur_img)
-cv2.imshow('Bright Luminance', bright_luminance_img)
-cv2.imshow('Dark Luminance', dark_luminance_img)
+# cv2.imshow('Originial', imgGray)
+# cv2.imshow('Bright Contrast', bright_contrast_img)
+# cv2.imshow('Dark Contrast', dark_contrast_img)
+# cv2.imshow('Blur', blur_img)
+# cv2.imshow('Bright Luminance', bright_luminance_img)
+# cv2.imshow('Dark Luminance', dark_luminance_img)
 
 
-result = ssim(imgGray, bright_contrast_img)
-result1 = ssim(imgGray, dark_contrast_img)
-result2 = ssim(imgGray, blur_img)
-result3 = ssim(imgGray, bright_luminance_img)
-result4 = ssim(imgGray, dark_luminance_img)
+# result = ssim(imgGray, bright_contrast_img)
+# result1 = ssim(imgGray, dark_contrast_img)
+# result2 = ssim(imgGray, blur_img)
+# result3 = ssim(imgGray, bright_luminance_img)
+# result4 = ssim(imgGray, dark_luminance_img)
 
-print('Result Bright Contrast:', result)
-print('Result Dark Contrast:', result1)
-print('Result Blur:', result2)
-print('Result Bright Luminance:' , result3)
-print('Result Dark Luminance:' , result4)
+# print('Result Bright Contrast:', result)
+# print('Result Dark Contrast:', result1)
+# print('Result Blur:', result2)
+# print('Result Bright Luminance:' , result3)
+# print('Result Dark Luminance:' , result4)
 
-cv2.waitKey()
+# cv2.waitKey()
 
 #Quellen:
 # https://scikit-image.org/docs/dev/auto_examples/transform/plot_ssim.html
@@ -74,40 +74,82 @@ cv2.waitKey()
 
 #Steganographie: Binärbild in Blau-Kanal
 
-grayImg = cv2.imread('Utils/th.jpg', cv2.IMREAD_COLOR)
+import cv2
+import numpy as np
 
-bitImages = []
-for bit in range(8):
-    bitMask = 1 << bit
-    bitPlane = cv2.bitwise_and(grayImg, bitMask)
-    bitImages.append(bitPlane)
+# Lade das Farbbild, in das du das andere Bild einbetten möchtest
+host_image = cv2.imread('Utils/th.jpg')
 
-for i, bitImages in enumerate(bitImages):
-    cv2.imwrite(f'Utils/bit_{i}.jpg', bitImages)
+# Lade das Bild, das du einbetten möchtest
+embedded_image = cv2.imread('Utils/bit_0Code.jpg')
 
-# Convert cover image to gray-scale
-cover = Image.open("Utils/th.jpg").convert('L')
-data_c = np.array(cover)
+# Überprüfe, ob die eingebettete Bilddatei in das Hostbild passt
+if embedded_image.shape[0] > host_image.shape[0] or embedded_image.shape[1] > host_image.shape[1]:
+    raise ValueError("Das eingebettete Bild ist zu groß, um in das Hostbild einzubetten.")
 
-# Convert image to 1-bit pixel, black and white and resize to cover image
-secret = Image.open("Utils/bit_0Code.jpg").convert('1')
-data_s = np.array(secret, dtype=np.uint8)
+# Iteriere durch die Pixel des eingebetteten Bildes und setze die LSBs der Pixel des Hostbildes
+for row in range(embedded_image.shape[0]):
+    for col in range(embedded_image.shape[1]):
+        for channel in range(embedded_image.shape[2]):
+            host_pixel = host_image[row, col, channel]
+            embedded_pixel = embedded_image[row, col, channel]
 
-# Rewrite LSB
-res = data_c & ~1 | data_s
+            # Lösche die letzten beiden Bits des Hostpixels und füge die beiden LSBs des eingebetteten Pixels hinzu
+            host_pixel = (host_pixel & 0b11111100) | (embedded_pixel >> 6)
 
-new_img = Image.fromarray(res).convert('RGB')
-new_img.save("Utils/cover-secret1.jpg")
-rgb = cv2.imread("Utils/cover-secret1.jpg", cv2.IMREAD_COLOR)
+            # Aktualisiere das Pixel im Hostbild
+            host_image[row, col, channel] = host_pixel
 
-cv2.imshow("Original", grayImg)
-cv2.imshow("Original + Code", rgb)
+# Speichere das modifizierte Hostbild mit dem eingebetteten Bild
+cv2.imwrite('Utils/result_image.jpg', host_image)
 
-#result = ssim(grayImg, secret_img)
+final_img = cv2.imread('Utils/result_image.jpg', cv2.IMREAD_COLOR)
+img = cv2.imread('Utils/th.jpg', cv2.IMREAD_COLOR)
 
-#print('Result Bright Contrast:', result)
+cv2.imshow('Original', img)
+cv2.imshow('Code', final_img)
+
+# result = ssim(img, final_img)
+# print('Result Bright Contrast:', result)
 
 cv2.waitKey()
+
+# img = cv2.imread('Utils/th.jpg')
+# b, g, r = cv2.split(img)
+# img_b = b
+
+# bitImages = []
+# for bit in range(8):
+#     bitMask = 1 << bit
+#     bitPlane = cv2.bitwise_and(img_b, bitMask)
+#     bitImages.append(bitPlane)
+
+# for i, bitImages in enumerate(bitImages):
+#     cv2.imwrite(f'Utils/bit_{i}.jpg', bitImages)
+
+# # Convert cover image to gray-scale
+# cover = img_b
+# data_c = np.array(cover)
+
+# # Convert image to 1-bit pixel, black and white and resize to cover image
+# secret = cv2.imread("Utils/bit_0Code.jpg")
+# data_s = np.array(secret, dtype=np.uint8)
+
+# # Rewrite LSB
+# res = data_c & ~1 | data_s
+
+# new_img = Image.fromarray(res)
+# new_img.save("Utils/cover-secret1.jpg")
+# rgb = cv2.imread("Utils/cover-secret1.jpg", cv2.IMREAD_COLOR)
+
+# cv2.imshow("Original", img)
+# cv2.imshow("Original + Code", rgb)
+# cv2.imshow("r", r)
+# #result = ssim(grayImg, secret_img)
+
+# #print('Result Bright Contrast:', result)
+
+# cv2.waitKey()
 
 #Quellen:
 # https://medium.com/@stephanie.werli/image-steganography-with-python-83381475da57
